946848a36300a813985102bea177c75d80447a8e
Anton Mokrushin <anton@mokr.org>
2018-04-04 05:11:59 +0300

    fix off-by-one error

--- lua/msgerrors.lua
+++ lua/msgerrors.lua
@@ -1,13 +1,13 @@
 local store_key = '${ joinKey(keyPrefix, keyStore) }'
 local nacked_items_key = '${ joinKey(keyPrefix, keyNackedItems) }'
-local limit = ARGV[1]
+local limit = tonumber(ARGV[1])
 
-local message_ids = redis.call('zrange', nacked_items_key, 0, limit)
+local message_ids = redis.call('zrange', nacked_items_key, 0, limit - 1)
 
 if next(message_ids) ~= nil then
     local messages = redis.call('hmget', store_key, unpack(message_ids))
     redis.call('hdel', store_key, unpack(message_ids))
-    redis.call('zremrangebyrank', nacked_items_key, 0, limit)
+    redis.call('zremrangebyrank', nacked_items_key, 0, limit - 1)
     return messages
 end
 
